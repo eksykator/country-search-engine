d0a27b9136c563ef87f5f75bfd8073afa9bfe5ba
Marta Bobowska <martabobowska2@interia.pl>
2018-06-24 19:18:01 +0200

    Add country code

--- js/script.js
+++ js/script.js
@@ -18,11 +18,22 @@ function searchCountries() {
 
 function showCountriesList(resp) {
     countriesList.innerHTML = '';
+    
+    var countryTemplate = document.getElementById('template').innerHTML;
+    Mustache.parse(countryTemplate);
+    
     resp.forEach(function(item) {
         var liElement = document.createElement('li');
-        var countryTemplate = document.getElementById('template').innerHTML;
-        Mustache.parse(countryTemplate);
-        var dataCountry = {name: item.name, capital: item.capital, area: item.area, currency: item.currencies[0], population: item.population, region: item.region};
+        
+        var dataCountry = {
+            name: item.name,
+            capital: item.capital,
+            area: item.area, 
+            population: item.population, 
+            region: item.region,
+            alpha3Code: item.alpha3Code
+        };
+        
         liElement.innerHTML = Mustache.render(countryTemplate, dataCountry);
         countriesList.appendChild(liElement);
     });
